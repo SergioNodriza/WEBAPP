3efb9110f6385af925d538cee22607bd7ef06bb5
SergioNodriza <sergio.gomez@nodriza.com>
2020-10-01 09:43:57 +0200

    Objetos Modelo-Controlador

--- app/controllers/listItems.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once("../models/listItems.php");
-require_once("../models/helpLimpiar.php");
-require_once("../models/selects.php");
-require_once("../views/listItems.php");
--- app/controllers/logOut.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once("../models/logOut.php");
-
-(new logOut)->cierre();
